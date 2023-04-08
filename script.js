f4c446423903e970a0cd549d7450244b2efc8e86
NabilaWardana <20081010114@student.upnjatim.ac.id>
2023-04-08 22:58:53 +0700

    add navbar

---
+++ script.js
@@ -0,0 +1,7 @@
+for(let i=0;i<=8;i++){
+    document.getElementById("container-card").innerHTML += `
+        <div class="card">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, aspernatur.</p>
+        </div>
+    `
+}
