924aecd923a07fdca72b750b0f05c63a11ec547e
NabilaWardana <20081010114@student.upnjatim.ac.id>
2023-04-10 00:36:10 +0700

    add footer

--- script.js
+++ script.js
@@ -5,3 +5,48 @@ for(let i=0;i<=8;i++){
         </div>
     `
 }
+
+
+function handleGetFormData(){
+    const name = document.getElementById("name").value;
+    const city = document.getElementById("city").value;
+    const email = document.getElementById("email").value;
+    const zipCode = document.getElementById("zip-code").value;
+    const status = document.getElementById("status").checked;
+    return{
+        name,
+        city,
+        email,
+        zipCode,
+        status
+    }
+}
+
+function isNumber(zipCode){
+    return isNaN(zipCode) ? false : true;
+}
+
+function checkboxIsChecked(){
+    return document.getElementById("status").checked;
+}
+
+function validateFormData(obj){
+    if(obj!==null && isNumber(obj.zipCode) && checkboxIsChecked()){
+        return true;
+    }
+    return false;
+}
+
+function submit(){
+    if(validateFormData(handleGetFormData())){
+        document.getElementById("warning").innerText="";
+    }else{
+        document.getElementById("warning").innerText="Periksa form Anda sekali lagi";
+    }
+}
+
+function submitForm(){
+    event.preventDefault(); // menghentikan pengiriman formulir
+    submit(); // memvalidasi formulir
+}
+
